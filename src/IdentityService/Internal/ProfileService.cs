2343e7576921c9c6b492812094058ba3142b71df
CodeFuller <CodeFuller@gmail.com>
2022-05-28 21:34:52 +0200

    Extend user claims with user id
    https://docs.duendesoftware.com/identityserver/v6/fundamentals/claims/

---
+++ src/IdentityService/Internal/ProfileService.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Test;
+
+namespace IdentityService.Internal
+{
+	internal class ProfileService : IProfileService
+	{
+		private readonly TestUserStore userStore;
+
+		public ProfileService(TestUserStore userStore)
+		{
+			this.userStore = userStore ?? throw new ArgumentNullException(nameof(userStore));
+		}
+
+		public Task GetProfileDataAsync(ProfileDataRequestContext context)
+		{
+			var user = userStore.FindBySubjectId(context.Subject.GetSubjectId());
+
+			var claims = new[]
+			{
+				new Claim(ClaimTypes.NameIdentifier, user.SubjectId),
+			};
+
+			context.IssuedClaims.AddRange(claims);
+
+			return Task.CompletedTask;
+		}
+
+		public Task IsActiveAsync(IsActiveContext context)
+		{
+			context.IsActive = true;
+
+			return Task.CompletedTask;
+		}
+	}
+}
--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -1,16 +1,19 @@
 using Duende.IdentityServer;
 using IdentityService;
+using IdentityService.Internal;
 using IdentityService.Pages;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 
-builder.Services.AddIdentityServer()
+builder.Services
+	.AddIdentityServer()
 	.AddInMemoryIdentityResources(Config.IdentityResources)
 	.AddInMemoryApiScopes(Config.ApiScopes)
 	.AddInMemoryClients(Config.Clients)
-	.AddTestUsers(TestUsers.Users);
+	.AddTestUsers(TestUsers.Users)
+	.AddProfileService<ProfileService>();
 
 builder.Services
 	.AddAuthentication()
