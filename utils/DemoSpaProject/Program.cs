6dd610f6776c09066d083ff780f998315626f0f3
CodeFuller <CodeFuller@gmail.com>
2022-08-25 08:22:53 +0200

    Enable code analysis for demo SPA project

--- utils/DemoSpaProject/Program.cs
+++ utils/DemoSpaProject/Program.cs
@@ -1,13 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -15,6 +11,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.MapFallbackToFile("index.html"); ;
+app.MapFallbackToFile("index.html");
 
 app.Run();
